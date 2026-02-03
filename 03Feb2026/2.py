8d9629ba9545e6380ce22f955c7dc8587512cff4
Kai <kaixiang980117@gmail.com>
2026-02-03 01:30:08 +0000

    done secoind try on coding

--- 03Feb2026/2.py
+++ 03Feb2026/2.py
@@ -1,29 +1,36 @@
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
 import art
 print(art.logo)
 
-def find_highest(bidding_dict):
+def highest_bidder(bidding_dict):
     winner = ""
     highest = 0
 
     for key in bidding_dict:
-        bid_amount = bidding_dict[key]
-        if bid_amount > highest:
-            highest = bid_amount
+        their_bids = bidding_dict[key]
+        if their_bids > highest:
+            highest = their_bids
             winner = key
-
-    print(f"Winner is {winner} with bid amount ${bid_amount}.")
+    
+    print(f"The winner is {winner} with bid amount ${highest}")
 
 dict = {}
-continue_bid = True
+cont = True
 
-while continue_bid:
-    name = input("What is your name?: ")
+while cont:
+    name = input("What is your name? ")
     bid = int(input("What is your bid? $"))
     dict[name] = bid
-    more_bidder = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    choice = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
 
-    if more_bidder == "no":
-        continue_bid = False
-        find_highest(dict)
+    if choice == "no":
+        cont = False
+        highest_bidder(dict)
+
     else:
+        cont = True
         print("\n" * 20)
