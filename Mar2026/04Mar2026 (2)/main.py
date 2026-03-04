5f24a3a8ac8f94aeaa975c4c18bd29f461cc7283
Kai <kaixiang980117@gmail.com>
2026-03-04 04:27:53 +0000

    more update on codes

--- Mar2026/04Mar2026 (2)/main.py
+++ Mar2026/04Mar2026 (2)/main.py
@@ -10,6 +10,8 @@ screen.tracer(0)
 
 player = Player()
 
+car_manager = CarManager()
+
 screen.listen()
 screen.onkey(player.move, "Up")
 
@@ -17,3 +19,12 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
