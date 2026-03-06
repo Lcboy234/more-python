88d3409d93f297d92eaef1ceca21ab51b36fcff5
Kai <kaixiang980117@gmail.com>
2026-03-06 01:00:34 +0000

    update for the day

--- Mar2026/06Mar2026/main.py
+++ Mar2026/06Mar2026/main.py
@@ -1,13 +1,58 @@
-# with open("weather_data.csv") as data_file:
-#     data_file = data_file.readlines()
-#     print(data_file)
+# # # import csv
 
-import csv
+# # # with open("weather_data.csv") as data_file:
+# # #     data = csv.reader(data_file)
+# # #     temperatures = []
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-    print(temperature)
+# # #     for row in data:
+# # #         if row[1] != "temp":
+# # #             temperatures.append(int(row[1]))
+
+# # #     print(temperatures)
+
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# # print(type(data))
+# # print(data)
+# # print(data["temp"])
+
+# # data_dict = data.to_dict()
+# # print(data_dict)
+
+# # temp_list = data["temp"].to_list()
+
+# # average = sum(temp_list) / len(temp_list)
+# # print(temp_list)
+
+# # # print(data["temp"].mean())
+# # # print(data["temp"].max())
+# # # print(data["temp"].min())
+# # # print(data["temp"].median())
+
+# # print(data["condition"])
+# # print(data.condition)
+
+# # search for column for Monday and return Row
+# # print(data[data.day == "Monday"])
+
+# # print(data[data.temp == data.temp.max()])
+
+# # don't print, use monday as variable and save the row inside
+# # monday = data[data.day == "Monday"]
+
+# # print(monday.temp)
+
+# # monday_temp = monday.temp[0]
+# # monday_temp_F = monday_temp * 9 / 5 + 32
+# # print(monday_temp_F)
+
+# data_dict = {
+#     "people": ["Kai1", "Kai2", "Kai3"],
+#     "scores": [77, 56, 70]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
