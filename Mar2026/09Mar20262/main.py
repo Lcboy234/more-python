9711ed20118c40b9e47ca7273f8821167ffc99eb
Kai <kaixiang980117@gmail.com>
2026-03-09 06:39:21 +0000

    update

--- Mar2026/09Mar20262/main.py
+++ Mar2026/09Mar20262/main.py
@@ -1,5 +1,58 @@
+from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_input.get()
+    email_username = email_username_input.get()
+    password = password_input.get()
+
+    with open("data.txt", "a") as f:
+        f.write(f"{website} | {email_username} | {password}\n")
+    
+        website_input.delete(0, END)
+        password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+# image
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+password_img = PhotoImage(file="logo.png")
+# (100, 100) = center
+canvas.create_image(100, 100, image=password_img)
+canvas.grid(column=1, row=0)
+
+# words
+website_word = Label(text="Website:", bg="white")
+website_word.grid(column=0, row=1)
+
+email_username_word = Label(text="Email/Username:", bg="white")
+email_username_word.grid(column=0, row=2)
+
+password_word = Label(text="Password:", bg="white")
+password_word.grid(column=0, row=3)
+
+# inputs
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2, sticky="W")
+website_input.focus()
+
+email_username_input = Entry(width=35)
+email_username_input.grid(column=1, row=2, columnspan=2, sticky="W")
+email_username_input.insert(0, "kaixiang980117@gmail.com")
+
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3, columnspan=2, sticky="W")
+
+# buttons
+generate_password_button = Button(text="Generate Password", bg="white")
+generate_password_button.grid(column=2, row=3, sticky="W")
+
+add_button = Button(text="Add", width=36, bg="white", command=save)
+add_button.grid(column=1, row=4, columnspan=2, sticky="W")
+
+window.mainloop()
